adebc25effcc2402000fdab4b01f3b56c68136d6
rauelguimmayen <rauelguimmayen@gmail.com>
2026-06-05 12:37:47 +0800

    updated base url

--- src/main.js
+++ src/main.js
@@ -92,4 +92,7 @@ const router = createRouter({
         ]
 })
 
-createApp(App).use(createPinia()).use(router).mount('#app')
+const app = createApp(App);
+app.use(createPinia());
+app.use(router);
+app.mount('#app');
